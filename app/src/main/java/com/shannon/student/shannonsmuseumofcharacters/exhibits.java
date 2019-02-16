e27935101b7766b2199cceda897b24e685f42d63
Sharvey14 <brownieb2411@gmail.com>
2019-02-16 15:47:19 -0800

    Initial commit

---
+++ app/src/main/java/com/shannon/student/shannonsmuseumofcharacters/exhibits.java
@@ -0,0 +1,13 @@
+package com.shannon.student.shannonsmuseumofcharacters;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class exhibits extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exhibits);
+    }
+}
